eade2edcdb5f6dbf36a32106bce544c39629da99
YS Yau <yau.yik.shiung@gmail.com>
2019-01-13 20:16:23 +0800

    add post-massage for testing

---
+++ post-message/receiver.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	// Get a reference to the <div> on the page that will display the
+	// message text.
+	var messageEle = document.getElementById('message');
+
+	// A function to process messages received by the window.
+	function receiveMessage(e) {
+		console.log("receiveMessage");
+		console.log(e);
+		// Update the div element to display the message.
+		messageEle.innerHTML = "Message Received: " + e.data;
+	}
+
+	// Setup an event listener that calls receiveMessage() when the window
+	// receives a new MessageEvent.
+	window.addEventListener('message', receiveMessage);
+}
