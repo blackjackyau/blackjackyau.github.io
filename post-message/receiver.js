6268d3acacd645b7131fc0a6521948f055b89fbb
YS Yau <yau.yik.shiung@gmail.com>
2019-01-13 20:31:11 +0800

    adding postmessage back to parent

--- post-message/receiver.js
+++ post-message/receiver.js
@@ -2,6 +2,7 @@ window.onload = function() {
 	// Get a reference to the <div> on the page that will display the
 	// message text.
 	var messageEle = document.getElementById('message');
+	var mainWin = (window.opener) ? window.opener : window.parent;
 
 	// A function to process messages received by the window.
 	function receiveMessage(e) {
@@ -9,6 +10,7 @@ window.onload = function() {
 		console.log(e);
 		// Update the div element to display the message.
 		messageEle.innerHTML = "Message Received: " + e.data;
+		mainWin.postMessage("Message Received Acknowledgement", "*");
 	}
 
 	// Setup an event listener that calls receiveMessage() when the window
